2c44d90a2226cad080643a09865b8fcdd8249dc4
Maurilio Ayrton Teotonio Alves <maurilio.alves@mctic.gov.br>
2024-07-08 14:37:54 -0300

    adicionando nova função que calcula o valor gasto para uma viagem

---
+++ funcoes/funcao-calculando-valorgasolina.js
@@ -0,0 +1,32 @@
+/*Objetivo:
+Desenvolver uma função chamada calcularValorGasto que recebe os seguintes parâmetros:
+
+kmRodado - O valor do km rodado.
+precoGasolina - O Valor do preço da Gasolina
+distanciaPecorrida - O valor da distancia a percorrer
+
+O que preciso saber? 
+1 - quantos Km o carro faz com um litro? 
+2 - qual o preço da gasolina?
+3 - qual a distancia percorrida?
+
+*/
+
+
+
+function calcularKM(distanciaPecorrida,kmPorLitro){
+    return distanciaPecorrida/kmPorLitro
+}
+
+function calcularValorGasto(calculoKM,precoGasolina){
+    return calculoKM*precoGasolina
+}
+
+
+let precoGasolina = 5.89;
+let distanciaPecorrida = 12.5;
+let kmPorLitro = 12.5;
+let calculoKM = calcularKM(distanciaPecorrida,kmPorLitro);
+let valorGasto = calcularValorGasto(calculoKM,precoGasolina);
+
+console.log (`olá para pecorrer ${distanciaPecorrida}km, seu carro fazendo ${kmPorLitro} KM/L e o preço da gasolina a R$${precoGasolina}, você vai gastar R$${valorGasto}`);
